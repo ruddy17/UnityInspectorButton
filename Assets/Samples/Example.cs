08c3109f4f9202a7f4b65d6a28d4fed167175bb5
Petr Sevostianov <ps@antilatency.com>
2023-07-06 15:44:04 +0300

    *

---
+++ Assets/Samples/Example.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Example : MonoBehaviour {
+
+    public InspectorButton _Print;
+
+    public void Print() {
+        Debug.Log("Hello");
+    }
+
+}
